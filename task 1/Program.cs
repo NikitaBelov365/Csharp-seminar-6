eb6dbdfd066101b150f9bf86523417bceb1cbb06
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-01 16:21:51 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 1: Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+int InputInt(string msg)
+{
+    System.Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] Reverse(int[] array)
+{
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        Swap(array, i, array.Length - 1 - i);
+    }
+    return array;
+}
+
+void Swap(int[] array, int i, int j)
+{
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+void Swap2(int[] array, int i, int j)
+{
+    (array[i], array[j]) = (array[j], array[i]);
+}
+
+int size = InputInt("Input array length");
+int[] array = Array(size);
+PrintArray(array);
+PrintArray(Reverse(array));
